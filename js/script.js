635a703fd6a5de7ab4a9a56bf655cb45573d3642
male8769 <malene_storgaard@live.dk>
2025-01-27 09:46:51 +0100

    yes

---
+++ js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const welcome = `I'm learning to code with JavaScript`;
+
+console.log(welcome);
+
+const welcome = `I want to become a good JavaScript programmer`;
+
+console.log(welcome);
+
+
+// ## JavaScript Task:
+
+
+// 3. Create a constant (`const`) variable to hold a greeting message. For example:
+
+//    ```html
+//    "I'm learning to code with JavaScript";
+//    ```
+
+// 4. Attempt to reassign the value of the const variable to:
+
+//    ```html
+//    "I want to become a good JavaScript programmer";
+//    ```
+
+// 5. Use `console.log` to observe what happens.
+
+// **Expected Result:**
+
+// A JavaScript error will be thrown: "Uncaught TypeError: Assignment to constant variable."
+// This demonstrates that const variables cannot be reassigned.
